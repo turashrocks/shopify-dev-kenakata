2bf05f8649403be0ba0c3f0f0b892a335544c9b7
Willkommen <turash.chowdhury@gmail.com>
2022-06-13 16:47:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: [],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,js,liquid}"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
